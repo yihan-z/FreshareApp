8291d211db291e7a98044125fe1347a29711be26
yihan-z <ybzhuang25@gmail.com>
2017-03-06 11:14:42 -0600

    Initial Commit

---
+++ FreshareApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  FreshareApp
+//
+//  Created by ZhuangYihan on 3/6/17.
+//  Copyright © 2017 ZhuangYihan. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
