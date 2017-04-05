8c05761e97a4fc4f00584a1de9ee04c3214ddafb
yihan-z <ybzhuang25@gmail.com>
2017-04-05 16:51:16 -0500

    merged with map Optional fix

--- FreshareApp/MapViewController.swift
+++ FreshareApp/MapViewController.swift
@@ -5,7 +5,6 @@
 //  Created by Labuser on 3/20/17.
 //  Copyright © 2017 ZhuangYihan. All rights reserved.
 //
-
 import UIKit
 import GoogleMaps
 import GooglePlaces
@@ -15,7 +14,6 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
     
     @IBOutlet var mapView: GMSMapView!
     
-    /*
     var locationManager = CLLocationManager()
     
     var didFindMyLocation = false
@@ -24,32 +22,31 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
     //var mapView: GMSMapView!
     var placesClient: GMSPlacesClient!
     var zoomLevel: Float = 15.0
-    
+    var longitude : Double = -90.301656
+    var latitude : Double = 38.648930
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        
         
         
         //Find current position
         locationManager = CLLocationManager()
-      
-        locationManager.requestWhenInUseAuthorization()
-        //locationManager.requestAlwaysAuthorization()
-       
         
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestAlwaysAuthorization()
         
         
         
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.distanceFilter = 50
         locationManager.delegate = self
-         locationManager.startUpdatingLocation()
+        locationManager.startUpdatingLocation()
         
         
         
         
-
+        
         if(CLLocationManager.authorizationStatus()==CLAuthorizationStatus.authorizedWhenInUse ||
             CLLocationManager.authorizationStatus() == CLAuthorizationStatus.authorizedAlways){
             currentLocation = locationManager.location
@@ -58,22 +55,32 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
             print("Error in finding location, did you deny access?")
             //exit()
         }
-        //Todo Put in Optional Check
-        let camera: GMSCameraPosition = GMSCameraPosition.camera(withLatitude: currentLocation!.coordinate.latitude, longitude: currentLocation!.coordinate.longitude,                                             zoom: zoomLevel)
+        //Default location (without authorization access) is STL
+        
+        if currentLocation != nil { //TODO Reset
+            print("not nil")
+            longitude = currentLocation!.coordinate.longitude
+            latitude = currentLocation!.coordinate.latitude
+        } else { // STL
+            longitude = -90.301656
+            latitude = 38.648930
+        }
+        
+        let camera: GMSCameraPosition = GMSCameraPosition.camera(withLatitude: latitude, longitude: longitude,  zoom: zoomLevel)
         let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
         view = mapView
         
         view.addObserver(self, forKeyPath: "myLocation", options: NSKeyValueObservingOptions.new, context: nil)
-    
+        
         
         
         // Creates a marker in the center of the map.
         let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: (currentLocation?.coordinate.latitude)!, longitude: (currentLocation?.coordinate.longitude)!)
+        marker.position = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
         marker.title = "Your Location"
         marker.snippet = NSLocale.current.regionCode
         marker.map = mapView
-
+        
     }
     
     override func didReceiveMemoryWarning() {
@@ -86,7 +93,6 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
             mapView.isMyLocationEnabled = true
         }
     }
-    */
+    
     
 }
-
